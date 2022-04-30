139e085d45d8497e1d4697f911dcd3bdcc257c2a
Tuag <igortuag@gmail.com>
2022-04-30 10:47:01 -0300

    add rotuer config

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import CountriesList from "./CountriesList";
+import Country from "./Country";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Countries Of The World</h1>
-        </header>
+        <BrowserRouter>
+          <div>
+            <header className="App-header">
+              <h1 className="App-title">Countries Of The World</h1>
+            </header>
+
+            <main className="AppBody">
+              <Switch>
+                <Route exact path="/" component={CountriesList} />
+                <Route path="/country/:name" component={Country} />
+              </Switch>
+            </main>
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
